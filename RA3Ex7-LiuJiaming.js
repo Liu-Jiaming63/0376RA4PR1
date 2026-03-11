c43ef4f9a308e79af36314489b5e3a24413d653f
LiuJiaming <jliu25@ilb.cat>
2026-03-11 12:39:39 +0100

    Init projecte

---
+++ RA3Ex7-LiuJiaming.js
@@ -0,0 +1,27 @@
+//1 - Selecció: Usa querySelector per canviar el color d'un <h1> quan la pàgina carregui.
+
+/*let element = document.querySelector("#titol-principal");
+console.log(titol)
+element.style.color = "blue";*/
+
+//2 - Contingut: Fes que un paràgraf <p> mostri "Hola Món" usant textContent.
+
+let change = document.textContent ("paragraf-hola");
+change.textContent = ("Hola Món");
+
+//3 - Atributs: Canvia la 'src' d'una imatge <img> usant setAttribute.
+fk
+
+//4 - Estils: Canvia el color de fons (backgroundColor) d'un element en fer-li clic.
+
+
+//5 - Classes: Crea un botó que afegeixi/tregui la classe 'actiu' amb classList.toggle.
+
+
+//6 - Esdeveniments: Afegeix un addEventListener a un botó perquè tregui una alerta (alert).
+
+
+//7 - Estructura: Crea un nou <li> amb createElement i afegeix-lo a una <ul> amb appendChild.
+
+
+//8 - Eliminació: Fes que un element desaparegui en fer-li clic usant el mètode remove().
